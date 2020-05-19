29638e8895c2acaeb83d919736f0ac465d7e71e4
steven noyes <stevenmnoyes.af@gmail.com>
2020-05-18 22:31:56 -0700

    scroll effect over code

---
+++ scriptScroll.js
@@ -0,0 +1,18 @@
+let scroll0 = document.querySelector('.scroll0');
+let scrolli = document.querySelector('.scrolli');
+
+
+
+function openScroll() {
+    scrolli.textContent = "Is it in?";
+}
+
+
+
+window.addEventListener('scroll', openScroll(scroll0) {
+    scrolli.textContent = "Is it in?";
+});
+
+
+
+
